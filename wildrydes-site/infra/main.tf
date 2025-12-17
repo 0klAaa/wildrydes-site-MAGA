9caf10f2b5e074fd6f674c4c7b2adc4803dc259b
0klAaa <mathieu.garcia@soprasteria.com>
2025-12-17 20:46:41 +0100

    init

---
+++ wildrydes-site/infra/main.tf
@@ -0,0 +1,29 @@
+resource "aws_amplify_app" "wildrydes" {
+  name       = "MAGA-wildrydes-site-Terraform"
+  repository = var.github_repo_url
+  access_token = var.github_token
+  build_spec = <<-YAML
+  version: 1
+  frontend:
+    phases:
+      build:
+        commands:
+          - echo "Static site build - nothing to compile"
+    artifacts:
+      baseDirectory: .
+      files:
+        - '**/*'
+    cache:
+      paths: []
+YAML
+    environment_variables = {
+    ENV = "prod"
+  }
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.wildrydes.id
+  branch_name = var.github_branch
+
+  enable_auto_build = true
+}
---
+++ wildrydes-site/infra/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "github_token" {
+  type      = string
+  sensitive = true
+  default = ""
+}
+
+variable "github_repo_url" {
+  type = string
+  default = "https://github.com/0klAaa/wildrydes-site-MAGA"
+}
+
+variable "github_branch" {
+  type    = string
+  default = "main"
+}
