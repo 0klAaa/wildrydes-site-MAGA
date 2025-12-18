13bf1e519215dc206d5c887a3363d33612f47f64
GARCIA Mathieu <mathieu.garcia@soprasteria.com>
2025-12-18 12:11:23 +0100

    fix iam role

--- wildrydes-site/infra/main.tf
+++ wildrydes-site/infra/main.tf
@@ -2,6 +2,8 @@ resource "aws_amplify_app" "wildrydes" {
   name       = "MAGA-wildrydes-site-Terraform"
   repository = var.github_repo_url
   oauth_token = var.github_token
+  iam_service_role_arn = aws_iam_role.MagaIAM.arn
+  
   build_spec = <<-YAML
   version: 1
   frontend:
@@ -28,13 +30,13 @@ resource "aws_amplify_branch" "main" {
   enable_auto_build = true
 }
 
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-
+resource "aws_iam_role" "MagaIAM" {
+  name = "MagaIAM"
+  
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
-    Version = "2025-18-12"
+    Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
