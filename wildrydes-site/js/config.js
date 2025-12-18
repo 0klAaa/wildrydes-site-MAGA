7f81e8d63618dfa54f224a0f9385e1948569dc07
GARCIA Mathieu <mathieu.garcia@soprasteria.com>
2025-12-18 16:31:37 +0100

    add user pool id / client

--- wildrydes-site/js/config.js
+++ wildrydes-site/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    // cognito: {
-    //     userPoolId: 'eu-west-3_hydVYJ9KM', 
-    //     userPoolClientId: 'rtma6u1rf62hah0ne34q62r4p', 
-    //     region: 'eu-west-3' 
-    // },
-    // api: {
-    //     invokeUrl: '', 
-    // }
+    cognito: {
+        userPoolId: '<OUTPUT_cognito_user_pool_id>', 
+        userPoolClientId: '<OUTPUT_cognito_user_pool_client_id>', 
+        region: 'eu-west-1' 
+    },
+    api: {
+        invokeUrl: '', 
+    }
 };
